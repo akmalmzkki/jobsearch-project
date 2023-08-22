9db6834731a2f0b139a9fc43b041945f9e69115a
akmalmzkki <akmalmzkki@student.telkomuniversity.ac.id>
2023-08-22 11:17:27 +0700

    .

--- src/Component/NavbarDiv/Navbar.jsx
+++ src/Component/NavbarDiv/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import {GiHamburgerMenu} from 'react-icons/gi'
+import { AiFillCloseCircle } from 'react-icons/ai'
 
 const Navbar = () => {
   const [navbarBg, setNavbarBg] = useState(false)
@@ -35,18 +36,22 @@ const Navbar = () => {
         <h1 className='logo text-[25px] text-blueColor z'><strong>Job</strong>Search</h1>
       </div>
       <div className="menu flex fixed right-0 gap-8 mr-10 md:flex md:mr-0 md:gap-0">
-        <button onClick={toggleMobileNav} className='md:hidden menu-list text-[#6f6f6f] hover:text-blueColor'>
+        <button onClick={toggleMobileNav} className={`md:hidden menu-list text-[#6f6f6f] hover:text-blueColor ${mobileNav ? 'hidden' : 'flex'}`}>
           <GiHamburgerMenu className='text-[25px]'/>
         </button>
       </div>
       <div className={`menu md:flex md:gap-8 md:mr-6 ${mobileNav ? 'block text-right' : 'hidden'}`}>
-        <li className='menu-list text-[#6f6f6f] hover:text-blueColor'>Jobs</li>
-        <li className='menu-list text-[#6f6f6f] hover:text-blueColor'>Companies</li>
-        <li className='menu-list text-[#6f6f6f] hover:text-blueColor'>About</li>
-        <li className='menu-list text-[#6f6f6f] hover:text-blueColor'>Contact</li>
-        <li className='menu-list text-[#6f6f6f] hover:text-blueColor'>Blog</li>
-        <li className='menu-list text-[#6f6f6f] hover:text-blueColor'>Login</li>
-        <li className='menu-list text-[#6f6f6f] hover:text-blueColor'>Register</li>
+        <button className='menu-list text-[#6f6f6f] hover:text-blueColor md:hidden'>
+          <AiFillCloseCircle className='text-[25px]' onClick={toggleMobileNav}/>
+        </button>
+        <br />
+        <li className='menu-list text-[#6f6f6f] hover:text-blueColor max-md:mt-2'>Jobs</li>
+        <li className='menu-list text-[#6f6f6f] hover:text-blueColor max-md:mt-2'>Companies</li>
+        <li className='menu-list text-[#6f6f6f] hover:text-blueColor max-md:mt-2'>About</li>
+        <li className='menu-list text-[#6f6f6f] hover:text-blueColor max-md:mt-2'>Contact</li>
+        <li className='menu-list text-[#6f6f6f] hover:text-blueColor max-md:mt-2'>Blog</li>
+        <li className='menu-list text-[#6f6f6f] hover:text-blueColor max-md:mt-2'>Login</li>
+        <li className='menu-list text-[#6f6f6f] hover:text-blueColor max-md:mt-2'>Register</li>
       </div>
     </div>
   )

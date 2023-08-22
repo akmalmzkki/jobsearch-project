511aee63a65e4e32aaeee8d9788d6ed1f3571253
akmalmzkki <akmalmzkki@student.telkomuniversity.ac.id>
2023-08-22 11:38:41 +0700

    .

--- src/Component/SearchDiv/Search.jsx
+++ src/Component/SearchDiv/Search.jsx
@@ -8,7 +8,7 @@ const Search = () => {
   return (
     <div className='search grid gap-7 bg-greyIsh rounded-[10px] p-12 mt-[150px]'>
       <form action="">
-        <div className='first-div flex justify-between items-center rounded-[8px] gap-6 bg-white p-5 shadow-lg shadow-greyIsh-700'>
+        <div className='first-div flex max-md:flex-col justify-between md:items-center rounded-[8px] gap-6 bg-white p-5 shadow-lg shadow-greyIsh-700'>
           <div className='flex gap-2 items-center'>
             <AiOutlineSearch className='text-[#6f6f6f] text-[25px] icon'/>
             <input type="text" className='bg-transparent text-blue-500 focus:outline-none w-[100%]' placeholder='Search job here....'/>
@@ -27,8 +27,8 @@ const Search = () => {
           <button className="bg-blueColor h-full py-3 px-8 rounded-[10px] text-white border-[2px] cursor-pointer hover:bg-white hover:text-textColor">Search</button>
         </div>
       </form>
-      <div className="sec-div flex flex-wrap items-center gap-12 justify-center">
-        <div className="single-search flex items-center gap-2">
+      <div className="sec-div flex max-md:flex-col max-md:gap-5 md:items-center md:gap-12 justify-center">
+        <div className="single-search justify-between flex items-center gap-2">
           <label htmlFor="relevance" className='text-[#808080] font-medium'>Sort by:</label>
           <select name="" id="relevance" className='bg-white rounded px-4 py-1 text-sm'>
             <option value="">Relevance</option>
@@ -37,7 +37,7 @@ const Search = () => {
             <option value="">Contains</option>
           </select>
         </div>
-        <div className="single-search flex items-center gap-2">
+        <div className="single-search justify-between flex items-center gap-2">
           <label htmlFor="type" className='text-[#808080] font-medium'>Type:</label>
           <select name="" id="type" className='bg-white rounded px-4 py-1 text-sm'>
             <option value="">Full-time</option>
@@ -46,7 +46,7 @@ const Search = () => {
             <option value="">Part-time</option>
           </select>
         </div>
-        <div className="single-search flex items-center gap-2">
+        <div className="single-search justify-between flex items-center gap-2">
           <label htmlFor="level" className='text-[#808080] font-medium'>Level:</label>
           <select name="" id="level" className='bg-white rounded px-4 py-1 text-sm'>
             <option value="">Senior</option>
